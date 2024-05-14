660d227c55daf14c1fcb878e4d4318ef994dec71
TheJadeEmperor <TheJadeEmperor@users.noreply.github.com>
2024-05-13 22:00:34 -0400

    1

--- header.php
+++ header.php
@@ -1,3 +1,137 @@
+<?php
+function gallery_function($atts) {
+  $anime = $atts['project_folder'];
+  $postTitle = $atts['project_name'];
+
+  $divID = str_replace('/', '_',  $anime);
+
+  $directory = $anime;
+
+  //valid image extensions
+  $validFiles = array('jpg', 'jpeg', 'png');
+  
+  $counter = 1; //images counter
+  if(is_dir($directory))
+  if ($handle = opendir($directory)) { //read all files in directory
+      //List all the files
+      while (false !== ($file = readdir($handle))) {
+          $ext = pathinfo($file, PATHINFO_EXTENSION);
+          if(in_array($ext, $validFiles)) {   
+              $small[$counter] = $file; //add image to array
+              $counter++; //increase counter
+          }
+      }//while
+      closedir($handle);
+  }//if
+
+  sort($small); //sort image names in order
+
+  //sorting adds a 0 element and shifts all elements back 1
+  //this will fix the array 
+  foreach ($small as $num => $picture) {
+      $small[$num+1] = $picture; 
+  }
+  unset($small[0]); //delete 0 element
+
+  //    print_r($small); exit;
+
+  //check for thumbnails
+  if(is_dir($directory.'/thumbnails')) $showThumbnails = 1;
+
+  $galleryContent .= '
+<div>
+<ul class="hoverbox">';
+    
+  foreach($small as $num => $picture) {
+    //$num = $num + 1; //offset the 0 element    
+    list($name, $ext) = explode('.', $picture); 
+    
+            if($showThumbnails) {
+                $readThisImg = $directory.'/'.$picture;
+                $showThisImg = $directory.'/thumbnails/'.$picture;
+            }
+            else    
+                $showThisImg = $readThisImg = $directory.'/'.$picture;
+      //$showThisImg = $site_url.'/'.$directory.'/'.$picture;
+      
+      if(file_exists($readThisImg)) {
+        list($width, $height, $type, $attr) = getimagesize($readThisImg);
+
+        if($thumbnails == 1) {
+          $galleryContent .= '<li title="'.$anime.'" onclick="openModal(\''.$divID.'\');currentSlide('.$num.')"><a href="#">
+          <img src="'.$showThisImg.'" alt="'.$anime.'" class="episode_thumbnail" />
+          <img src="'.$showThisImg.'" class="preview_large" alt="'.$anime.'" >
+          </a></li>'; 
+        }
+        else {
+                    
+                    if($height > $width)
+                        $class = 'preview_tall';
+                    else
+                        $class = 'preview_portrait';
+        
+          $galleryContent .= '<li title="'.$anime.'" onclick="openModal(\''.$divID.'\');currentSlide('.$num.')"><a>
+          <img src="'.$showThisImg.'" alt="'.$anime.'" />
+          <img src="'.$showThisImg.'" class="'.$class.'" alt="'.$anime.'" >
+          </a></li>'; 
+        }					
+      }      
+    }//foreach
+
+    $galleryContent .= '</ul><div>';
+
+    $counter = $counter - 1; //JS arrays start at 0
+
+    //display the modal elements
+    $galleryContent .=  '<div id="'.$divID.'" class="modal">
+    <a class="close cursor" onclick="closeModal(\''.$anime.'\')">&times;</a>
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    <div class="modal-content">';
+
+    foreach($small as $num => $picture) {
+        $showThisImg = $directory.'/'.$picture;
+        $galleryContent .=  '<div class="mySlides">
+        <div class="numbertext">'.$num.' / '.$counter.'</div>
+        <img src="'.$showThisImg.'" onclick="plusSlides(1)" class="lightbox_main_image cursor">
+        </div>';
+    }
+    $galleryContent .=  '<div class="caption-container">
+    <p id="caption">'.$postTitle.'</p>
+    </div>
+    </div>
+    </div>';
+
+    return $galleryContent;
+  }
+
+
+function hoverbox_video ($atts) {
+
+  $directory = $atts['project_folder'];
+
+  $validFiles = array('mp4', 'webm', 'avi');
+
+  
+  if(is_dir($directory))
+  if ($handle = opendir($directory)) { //read all files in directory
+      //List all the files
+      while (false !== ($file = readdir($handle))) {
+          
+          $ext = pathinfo($file, PATHINFO_EXTENSION);
+          if(in_array($ext, $validFiles)) {   
+              
+             $return .='<video autoplay muted loop class="hoverbox_video">
+      <source src="'.$directory.'/'.$file.'" type="video/mp4" />
+  </video>';
+          }
+      }//while
+      closedir($handle);
+  }//if
+  return $return;
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,11 +148,9 @@
   <link rel="stylesheet" href="assets/link.css">
 
   <!-- hoverbox gallery -->
-  <!--
   <link rel="stylesheet" href="assets/hoverbox.css">
   <script src="assets/hoverbox.js"></script>
--->
- <!-- hoverbox gallery -->
+  <!-- hoverbox gallery -->
  
   </style>
 
@@ -57,14 +189,14 @@
 <header>
   <div class="menuContainer">
     <div class="logo">
-      <h1>MyCultureLink</h1> <!--<img src="images/logo.jpg" />-->
+      <h1>MyCultureLink</h1>
     </div>
     <nav>
       <ul>
-        <li><a href="#">Media</a></li>
-        <li><a href="#">Music</a></li>
-        <li><a href="#">Marketing</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#gallery">Gallery</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
     </nav>
   </div>
--- home.php
+++ home.php
@@ -26,13 +26,38 @@
 <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
 <div class="elfsight-app-7b1f289d-4ad0-446a-8543-6693f4038fd2" data-elfsight-app-lazy></div>
 
+<div class="row">
+    <div class="col-lg">
+      <h2>Media &#x2022; Music &#x2022; Marketing</h2>
+      <h1>Under Construction</h1>
+      <p>We are currently working on our website. Please check back later!</p>
+      <p>For business inquiries, please email us at <a href="mailto:dacapo5580@yahoo.com">dacapo5580@yahoo.com</a></p>
 
-  <h2>Media &#x2022; Music &#x2022; Marketing</h2>
-  <h1>Under Construction</h1>
-  <p>We are currently working on our website. Please check back later!</p>
-  <p>For business inquiries, please email us at <a href="mailto:dacapo5580@yahoo.com">dacapo5580@yahoo.com</a></p>
+    </div>
+  </div>
+ 
 
-     
+  <div class="row">
+    <div class="col-lg">
+      <h1>Gallery</h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg">
+  
+    <?php
+    
+      $atts = array(
+        'project_folder' => 'images/gallery',
+        'project_name' => 'MyCultureLink Gallery'
+      );
+
+      echo gallery_function($atts);
+
+    ?>
+    </div>
+  </div>
+  
 </div>
 
 
